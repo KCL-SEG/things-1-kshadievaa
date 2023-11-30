81ca046314a9c87a70e3a66dd4620210afe882d4
Kamila Shadieva <kamila.shadieva@kcl.ac.uk>
2023-11-30 00:12:32 +0000

    Description can be blank

--- things/models.py
+++ things/models.py
@@ -5,10 +5,10 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class Thing(models.Model):
     name = models.CharField(max_length = 30, unique = True, blank = False)
-    description = models.CharField(max_length = 120)
+    description = models.CharField(max_length = 120, blank = True)
     quantity = models.IntegerField(
         validators=[
-            MinValueValidator(0, message = None),
-            MaxValueValidator(100, message = None),
+            MinValueValidator(0, message = "Quantity must be not less than zero"),
+            MaxValueValidator(100, message = "Quantity must be less than 100"),
         ]
     )
